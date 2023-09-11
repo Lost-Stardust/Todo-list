fc38569c0e74123a0fc5f4150e4f4ffdb79daa72
Isa <Isaismail3000@gmail.com>
2023-09-11 23:00:10 +0300

    Remove console logs and add sidebarBtn to eventlistener

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,13 +1,16 @@
 const sidebarSlide = () => {
   const sidebarBtn = document.querySelector(".sidebar-btn");
   const sidebar = document.querySelector(".sidebar");
-  console.log(sidebar);
-  console.log(sidebarBtn);
   sidebarBtn.addEventListener("click", () => {
-    if (sidebar.classList.contains("inactive")) {
+    if (
+      sidebar.classList.contains("inactive") &&
+      sidebarBtn.classList.contains("inactive")
+    ) {
       sidebar.classList.remove("inactive");
+      sidebarBtn.classList.remove("inactive");
     } else {
       sidebar.classList.add("inactive");
+      sidebarBtn.classList.add("inactive");
     }
   });
 };
