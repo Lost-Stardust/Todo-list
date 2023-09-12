7e17c0f5a4e139563e9505951145b8d01d4c0f95
Isa <Isaismail3000@gmail.com>
2023-09-12 22:12:22 +0300

    Import and run projectPopup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import "./sidebar";
 import { sidebarSlide } from "./sidebar";
+import { projectPopup } from "./project-popup";
 sidebarSlide();
+projectPopup();
 console.log("meow meow");
