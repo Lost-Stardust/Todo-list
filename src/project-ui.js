a857e96d4dec708ee00b82ce91332e4464b4f608
Isa <Isaismail3000@gmail.com>
2023-09-17 21:44:23 +0300

    Make project a button instead of div

--- src/project-ui.js
+++ src/project-ui.js
@@ -1,7 +1,7 @@
 import { projectList } from "./add-project";
 const makeProject = () => {
   for (let i = 0; i < projectList.length; i++) {
-    let project = document.createElement("div");
+    let project = document.createElement("button");
     project.className = "project";
     project.dataset.index = projectList.indexOf(projectList[i]);
 
