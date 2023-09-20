68dee2bf6e8521958bd0e24e657f85a301ba5b76
Isa <Isaismail3000@gmail.com>
2023-09-20 15:05:56 +0300

    Import and call todoPopup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./styles.css";
 import "./sidebar";
 import { sidebarSlide } from "./sidebar";
 import { projectPopup } from "./project-popup";
+import { todoPopup } from "./todo-popup";
 sidebarSlide();
 projectPopup();
+todoPopup();
 console.log("meow meow");
