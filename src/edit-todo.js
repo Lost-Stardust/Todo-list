9fcaeaa3918189cc32fc048d6b3621a7e180ab20
Isa <Isaismail3000@gmail.com>
2024-01-03 03:59:59 +0300

    Select input elements from edit popup

--- src/edit-todo.js
+++ src/edit-todo.js
@@ -2,5 +2,11 @@ import { todoList } from "./add-todo";
 import { index } from "./add-todo";
 const editTodo = () => {
   console.log(todoList[index]);
+  const title = document.querySelector("#edit-title").value;
+  const desc = document.querySelector("#edit-desc").value;
+  const date = document.querySelector("#edit-date").value;
+  const priority = document.querySelector(".edit-priority").value;
+  const project = document.querySelector("#edit-projectSelect").value;
+  console.log({ title, desc, date, priority, project });
 };
 export { editTodo };
