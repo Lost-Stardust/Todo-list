92700e0ef29be7c83e5dd714681b19d96640c82c
isa <isaismail3000@gmail.com>
2024-05-10 16:56:01 +0300

    Separate ui logic from add-todo, place in todo-ui

--- src/add-todo.js
+++ src/add-todo.js
@@ -1,8 +1,6 @@
-import { editPopup } from "./edit-popup";
 const todoList = [
   // Todos made will be stored in here
 ];
-let index;
 
 const addTodo = () => {
   class Todo {
@@ -26,80 +24,6 @@ const addTodo = () => {
 
   todoList.push(todo1);
   console.log(todoList);
-
-  const todo = document.createElement("div");
-  todo.className = "todo";
-  const projectDisplay = document.querySelector(
-    `.${project}`.replace(/\s/g, "")
-  );
-  console.log(projectDisplay);
-  projectDisplay.appendChild(todo);
-  todo.dataset.index = todoList.length - 1;
-
-  const otherInfo = document.createElement("div");
-  todo.appendChild(otherInfo);
-  otherInfo.className = "other-info";
-
-  const titleBox = document.createElement("div");
-  todo.appendChild(titleBox);
-  titleBox.className = "title-box";
-
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.className = "checkbox";
-  titleBox.appendChild(checkbox);
-
-  const titleWrapper = document.createElement("div");
-  titleWrapper.className = "title-wrapper";
-  titleBox.appendChild(titleWrapper);
-
-  const todoTitle = document.createElement("p");
-  todoTitle.className = "todo-title";
-  todoTitle.textContent = todo1.title;
-  titleWrapper.appendChild(todoTitle);
-
-  const todoDesc = document.createElement("p");
-  todoDesc.className = "desc";
-  todoDesc.textContent = todo1.desc;
-  titleWrapper.appendChild(todoDesc);
-
-  const todoDate = document.createElement("p");
-  todoDate.className = "todo-date";
-  todoDate.textContent = todo1.date;
-  otherInfo.appendChild(todoDate);
-
-  const todoPriority = document.createElement("p");
-  todoPriority.className = "todo-priority";
-  todoPriority.textContent = todo1.priority;
-  otherInfo.appendChild(todoPriority);
-
-  const edit = document.createElement("img");
-  edit.className = "edit";
-  edit.src = "../visual-assets/editing.png";
-  otherInfo.appendChild(edit);
-
-  const trash = document.createElement("img");
-  trash.className = "trash";
-  trash.src = "../visual-assets/trash-bin.png";
-  otherInfo.appendChild(trash);
-
-  trash.addEventListener("click", (e) => {
-    e.target.parentNode.parentNode.parentNode.removeChild(
-      e.target.parentNode.parentNode
-    );
-    let index = e.target.parentNode.parentNode.dataset.index;
-    todoList.splice(index, 1);
-    console.log(todoList);
-  });
-
-  edit.addEventListener("click", (e) => {
-    let indexValue = e.target.parentNode.parentNode.dataset.index;
-    index = indexValue;
-    console.log(index);
-    console.log("edit listener triggered");
-    editPopup();
-  });
 };
 export { addTodo };
 export { todoList };
-export { index };
--- src/todo-ui.js
+++ src/todo-ui.js
@@ -1,17 +1,88 @@
+import { todoList } from "./add-todo";
+import { editPopup } from "./edit-popup";
 let todoInfo = {};
+let index;
 const makeTodo = () => {
   todoInfo.title = document.querySelector("#todo-title").value;
   todoInfo.desc = document.querySelector("#desc").value;
   todoInfo.priority = document.querySelector(".priority").value;
   todoInfo.date = document.querySelector("#date").value;
   todoInfo.project = document.querySelector("#projectSelect").value;
-  console.log(
-    todoInfo.title,
-    todoInfo.desc,
-    todoInfo.priority,
-    todoInfo.date,
-    todoInfo.project
+  console.log({ todoInfo });
+
+  const todo = document.createElement("div");
+  todo.className = "todo";
+  const projectDisplay = document.querySelector(
+    `.${todoInfo.project}`.replace(/\s/g, "")
   );
+  console.log(projectDisplay);
+  projectDisplay.appendChild(todo);
+  todo.dataset.index = todoList.length;
+
+  const otherInfo = document.createElement("div");
+  todo.appendChild(otherInfo);
+  otherInfo.className = "other-info";
+
+  const titleBox = document.createElement("div");
+  todo.appendChild(titleBox);
+  titleBox.className = "title-box";
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "checkbox";
+  titleBox.appendChild(checkbox);
+
+  const titleWrapper = document.createElement("div");
+  titleWrapper.className = "title-wrapper";
+  titleBox.appendChild(titleWrapper);
+
+  const todoTitle = document.createElement("p");
+  todoTitle.className = "todo-title";
+  todoTitle.textContent = todoInfo.title;
+  titleWrapper.appendChild(todoTitle);
+
+  const todoDesc = document.createElement("p");
+  todoDesc.className = "desc";
+  todoDesc.textContent = todoInfo.desc;
+  titleWrapper.appendChild(todoDesc);
+
+  const todoDate = document.createElement("p");
+  todoDate.className = "todo-date";
+  todoDate.textContent = todoInfo.date;
+  otherInfo.appendChild(todoDate);
+
+  const todoPriority = document.createElement("p");
+  todoPriority.className = "todo-priority";
+  todoPriority.textContent = todoInfo.priority;
+  otherInfo.appendChild(todoPriority);
+
+  const edit = document.createElement("img");
+  edit.className = "edit";
+  edit.src = "../visual-assets/editing.png";
+  otherInfo.appendChild(edit);
+
+  const trash = document.createElement("img");
+  trash.className = "trash";
+  trash.src = "../visual-assets/trash-bin.png";
+  otherInfo.appendChild(trash);
+
+  trash.addEventListener("click", (e) => {
+    e.target.parentNode.parentNode.parentNode.removeChild(
+      e.target.parentNode.parentNode
+    );
+    let index = e.target.parentNode.parentNode.dataset.index;
+    todoList.splice(index, 1);
+    console.log(todoList);
+  });
+
+  edit.addEventListener("click", (e) => {
+    let indexValue = e.target.parentNode.parentNode.dataset.index;
+    index = indexValue;
+    console.log(index);
+    console.log("edit listener triggered");
+    editPopup();
+  });
 };
 export { todoInfo };
 export { makeTodo };
+export { index };
