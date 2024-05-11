2e9d81875ed4388bee1e5aad5fe4a21cdbaa9c87
isa <isaismail3000@gmail.com>
2024-05-11 17:20:10 +0300

    Create all todos in localStorage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import "./sidebar";
 import { sidebarSlide } from "./sidebar";
 import { projectPopup } from "./project-popup";
 import { todoPopup } from "./todo-popup";
+import { makeTodo } from "./todo-ui";
+import { todoList } from "./add-todo";
 sidebarSlide();
 projectPopup();
 todoPopup();
-console.log("meow meow");
+
+// check if local storage's todoList array is empty
+const storedList = JSON.parse(localStorage.todoList);
+console.log(storedList);
+if (storedList.length !== 0) {
+  storedList.forEach((todo) => {
+    todoList.push(todo);
+    makeTodo(todo);
+  });
+}
