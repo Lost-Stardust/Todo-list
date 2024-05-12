7330b424f274c3101386b1fc08286190a00ad8ca
isa <isaismail3000@gmail.com>
2024-05-12 15:17:39 +0300

    Create new array for tracking projects, export it
    makeProject function now takes projectList as a param.

--- src/add-project.js
+++ src/add-project.js
@@ -5,18 +5,26 @@ let projectList = [];
 const defaultProject = {
   name: "Default",
 };
-// projectList.push(defaultProject);
-// makeProject();
+// projList that will not be emptied, will be used as
+// replacement for projectList in localStorage
+const projList = [];
+// add projList to localStorage if it doesnt exist
+if (!localStorage.getItem("projList")) {
+  localStorage.setItem("projList", JSON.stringify(projList));
+}
 const addProject = () => {
   const projectName = document.querySelector("#project-title").value;
   const project = {};
   project.name = projectName;
 
   projectList.push(project);
-  makeProject();
+  projList.push(project);
+  makeProject(projectList);
   projectList = [];
+  console.log(projList);
 };
 export { addProject };
 export { projectList };
+export { projList };
 
 // Project name will be taken from popup input
