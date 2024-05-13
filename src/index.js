31519a3a70e1bf8b8dfabfc2906d6f3d03327653
isa <isaismail3000@gmail.com>
2024-05-13 13:14:42 +0300

    Create projects from local storage on site load

--- src/index.js
+++ src/index.js
@@ -5,12 +5,25 @@ import { projectPopup } from "./project-popup";
 import { todoPopup } from "./todo-popup";
 import { makeTodo } from "./todo-ui";
 import { todoList } from "./add-todo";
+import { makeProject } from "./project-ui";
+import { projectList } from "./add-project";
 sidebarSlide();
 projectPopup();
 todoPopup();
 
-// check if local storage's todoList array is empty
-const storedList = JSON.parse(localStorage.todoList);
+// Retrieve stored projects and create them
+
+const storedProjs = JSON.parse(localStorage.getItem("projList"));
+if (storedProjs.length !== 0) {
+  storedProjs.forEach((proj) => {
+    projectList.push(proj);
+    console.log(projectList);
+    makeProject(proj);
+  });
+}
+
+// Retrieve stored todos and create them
+const storedList = JSON.parse(localStorage.getItem("todoList"));
 console.log(storedList);
 if (storedList.length !== 0) {
   storedList.forEach((todo) => {
