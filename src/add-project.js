8c09d14f571e0391d29d55fc73e679e832b64732
isa <isaismail3000@gmail.com>
2024-05-13 13:11:22 +0300

    Change how projects are made and use localStorage
    Projects were being made by iterating over an array but now will be made
    one at a time.

--- src/add-project.js
+++ src/add-project.js
@@ -1,30 +1,45 @@
 import { makeProject } from "./project-ui";
 // import { projectList } from ".";
 
-let projectList = [];
-const defaultProject = {
-  name: "Default",
-};
-// projList that will not be emptied, will be used as
-// replacement for projectList in localStorage
-const projList = [];
-// add projList to localStorage if it doesnt exist
-if (!localStorage.getItem("projList")) {
-  localStorage.setItem("projList", JSON.stringify(projList));
-}
+let projectList = [
+  // projects made will be stored here
+];
+
+// variable that will represent stored projectList in local storage
+let storedProjs;
+
 const addProject = () => {
+  class Project {
+    constructor(name) {
+      this.name = name;
+    }
+  }
   const projectName = document.querySelector("#project-title").value;
-  const project = {};
+
+  const project = new Project(projectName);
   project.name = projectName;
+  console.log(project);
 
   projectList.push(project);
-  projList.push(project);
-  makeProject(projectList);
-  projectList = [];
-  console.log(projList);
+  makeProject(project);
+
+  // Retrieve the projList in local storage
+  // Update it with the new project that is created
+
+  storedProjs = JSON.parse(localStorage.getItem("projList"));
+  storedProjs.push(project);
+  localStorage.setItem("projList", JSON.stringify(storedProjs));
+  console.log(storedProjs.length);
+
+  console.log(projectList);
 };
+
+// add projList to localStorage if it doesnt exist
+if (!localStorage.getItem("projList")) {
+  localStorage.setItem("projList", JSON.stringify(projectList));
+}
+
 export { addProject };
 export { projectList };
-export { projList };
 
 // Project name will be taken from popup input
--- src/project-ui.js
+++ src/project-ui.js
@@ -1,54 +1,47 @@
-import { projList } from "./add-project";
-const makeProject = (projectList) => {
-  for (let i = 0; i < projectList.length; i++) {
-    let project = document.createElement("button");
-    project.className = "project";
-    projList.forEach((proj) => {
-      project.dataset.index = projList.indexOf(proj);
+import { projectList } from "./add-project";
+const makeProject = (project) => {
+  let projectbtn = document.createElement("button");
+  projectbtn.className = "project";
+  projectbtn.dataset.index = projectList.indexOf(project);
+
+  const projectContainer = document.querySelector(".projects-container");
+  projectContainer.appendChild(projectbtn);
+
+  projectbtn.textContent = project.name;
+  console.log(projectbtn);
+
+  // Create div for each project to display its respective todos in
+  const pDisplay = document.createElement("div");
+  // remove whitespace from textcontent to use as class name
+  pDisplay.className = project.name.replace(/\s/g, "");
+  pDisplay.dataset.index = projectList.indexOf(project);
+
+  const content = document.querySelector(".content");
+  content.appendChild(pDisplay);
+  console.log(pDisplay);
+
+  // const defaultProject = document.querySelector(".Default");
+  // defaultProject.classList.add("active");
+  // console.log(defaultProject);
+
+  projectbtn.addEventListener("click", (e) => {
+    // remove whitespace from textContent to use as selector
+    const name = project.name.replace(/\s/g, "");
+    const pDisplay = document.querySelector(`.${name}`);
+
+    // Select all poject display divs and remove active class for all
+    const otherDisplays = content.querySelectorAll("div");
+    otherDisplays.forEach((d) => {
+      d.classList.remove("active");
     });
+    // assign active class to clicked project's div
+    pDisplay.classList.add("active");
+  });
 
-    const projectContainer = document.querySelector(".projects-container");
-    projectContainer.appendChild(project);
-
-    project.textContent = projectList[i].name;
-    console.log(project);
-
-    // Create div for each project to display its respective todos in
-    const pDisplay = document.createElement("div");
-    // remove whitespace from textcontent to use as class name
-    pDisplay.className = projectList[i].name.replace(/\s/g, "");
-    pDisplay.dataset.index = projectList.indexOf(projectList[i]);
-
-    const content = document.querySelector(".content");
-    content.appendChild(pDisplay);
-    console.log(pDisplay);
-
-    // const defaultProject = document.querySelector(".Default");
-    // defaultProject.classList.add("active");
-    // console.log(defaultProject);
-
-    project.addEventListener("click", (e) => {
-      // remove whitespace from textContent to use as selector
-      const name = project.textContent.replace(/\s/g, "");
-      const pDisplay = document.querySelector(`.${name}`);
-
-      // Select all poject display divs and remove active class for all
-      const otherDisplays = content.querySelectorAll("div");
-      otherDisplays.forEach((d) => {
-        d.classList.remove("active");
-      });
-      // assign active class to clicked project's div
-      pDisplay.classList.add("active");
-    });
-  }
-  const projectBtns = document.querySelectorAll(".project");
-  console.log([projectBtns]);
+  // Create and append remove button to project
   const remove = document.createElement("button");
   remove.className = "remove";
-
-  projectBtns.forEach((btn) => {
-    btn.appendChild(remove);
-  });
+  projectbtn.appendChild(remove);
 
   remove.addEventListener("click", (e) => {
     let name = e.target.parentNode.textContent.replace(/\s/g, "");
@@ -59,8 +52,8 @@ const makeProject = (projectList) => {
 
     let index = e.target.parentNode.dataset.index;
     console.log(index);
-    projList.splice(index, 1);
-    console.log(projList);
+    projectList.splice(index, 1);
+    console.log(projectList);
 
     // correct data index of project buttons
     let defaultp = projectContainer.querySelector(".project");
